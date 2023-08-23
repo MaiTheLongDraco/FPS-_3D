b8749f8b3303ffc73a40da0b197d4d6154f7f572
Mai Thế Long <128013309+MaiTheLongDraco@users.noreply.github.com>
2023-08-23 20:09:49 +0700

    Add gun switcher system

--- FPS Project/Assets/Script/Gun Control/GunSwitcher.cs
+++ FPS Project/Assets/Script/Gun Control/GunSwitcher.cs
@@ -5,4 +5,36 @@ using UnityEngine;
 public class GunSwitcher : MonoBehaviour
 {
     [SerializeField] private List<GameObject> guns;
+    private int gunIndex;
+    private void Start()
+    {
+        gunIndex = 0;
+        SwitchGun();
+    }
+
+    public void SwitchGun()
+    {
+        HandleGunIndex();
+        guns[gunIndex].SetActive(true);
+        for (int i = 0; i < guns.Count; i++)
+        {
+            if (i == gunIndex)
+            {
+                guns[i].SetActive(true);
+            }
+            else
+            {
+                guns[i].SetActive(false);
+            }
+        }
+    }
+    private void HandleGunIndex()
+    {
+        gunIndex++;
+        if (gunIndex >= guns.Count)
+        {
+            gunIndex = 0;
+        }
+    }
+
 }
