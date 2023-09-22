b7850c81f73e57b59608720fa04cda488c95ebd2
Mai Thế Long <128013309+MaiTheLongDraco@users.noreply.github.com>
2023-09-22 11:25:51 +0700

    Apply new asset

--- FPS Project/Assets/Script/Player Control/RotateByDrag.cs
+++ FPS Project/Assets/Script/Player Control/RotateByDrag.cs
@@ -1,21 +1,15 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class RotateByDrag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+public class RotateByDrag : MonoBehaviour, IDragHandler
 {
     [SerializeField] private Transform playerTran;
     [SerializeField] private Transform cameraTran;
-    [SerializeField] private Transform GunTran;
     [SerializeField] private float minPicht;
     [SerializeField] private float maxPicht;
     [SerializeField] private float rotationSpeed;
     float rotationX;
     float rotationY;
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        print("OnBeginDrag");
-    }
-
     public void OnDrag(PointerEventData eventData)
     {
         print("OnDrag");
@@ -24,18 +18,6 @@ public class RotateByDrag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEnd
         rotationX = Mathf.Clamp(rotationX, minPicht, maxPicht);
         playerTran.transform.Rotate(0, -rotationY, 0);
         Debug.Log($" rotationX {rotationX}");
-        // GunTran.transform.localRotation = Quaternion.Euler(-rotationX, 0, 0);
         cameraTran.transform.localRotation = Quaternion.Euler(-rotationX, 0, 0);
-    }
-    public void SetGunTranform(Gun currenGun)
-    {
-        print($" prev gun tranform -- {GunTran}");
-        GunTran = currenGun.transform;
-        print($" after gun tranform  --{GunTran}");
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        print("OnEndDrag");
     }
 }
