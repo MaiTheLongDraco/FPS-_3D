90d3ad68e5fa8c0c9dd4f5a76c47631bfc02e64b
Mai Thế Long <128013309+MaiTheLongDraco@users.noreply.github.com>
2023-10-04 10:13:57 +0700

    Make random rating UI and its anim

--- FPS Project/Assets/Script/Manager/Setting.cs
+++ FPS Project/Assets/Script/Manager/Setting.cs
@@ -1,22 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class Setting : MonoBehaviour
 {
     [SerializeField] private GameObject creditUI;
     [SerializeField] private GameObject settingUI;
+    [SerializeField] private GameObject ratingUI;
+    [SerializeField] private RectTransform ratingRect;
+    [SerializeField] private float random;
+    [SerializeField] private float transitionPointDown;
+    [SerializeField] private float transitionPointUp;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        ratingRect = ratingUI.GetComponent<RectTransform>();
+        SetActiveRandomRating();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+    private void SetActiveRandomRating()
+    {
+        var randomNB = Random.value;
+        random = randomNB;
+        if (randomNB <= 0.5f)
+        {
+            SetActiveRatingUI(true);
+        }
+    }
+    public void SetActiveRatingUI(bool set)
+    {
+        switch (set)
+        {
+            case true:
+                {
+                    ratingUI.GetComponent<RectTransform>().DOMoveY(transitionPointDown, 0.5f);
+                    ratingUI.SetActive(set);
+                }
+                break;
+            case false:
+                {
+                    ratingUI.GetComponent<RectTransform>().DOMoveY(transitionPointUp, 0.7f);
+                    StartCoroutine(RatingClose(0.7f, ratingUI));
+                }
+                break;
+
+        }
+    }
+    private IEnumerator RatingClose(float time, GameObject ratingPanel)
+    {
+        yield return new WaitForSeconds(time);
+        ratingPanel.SetActive(false);
     }
     public void SetActiveCredit(bool set)
     {
